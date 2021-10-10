c239c9e5ec826afa5175dc0d6d71fa47844e001e
jingjingxyk <zonghengbaihe521@qq.com>
2021-10-10 15:30:18 +0800

    [2021/10/10 15:30] Auto update by script

--- git-auto-commit.sh
+++ git-auto-commit.sh
@@ -6,5 +6,5 @@ __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__} &&
 git add -A && \
 git commit -m "[`date '+%Y/%m/%d %H:%M'`] Auto update by script" && \
-git push --set-upstream origin master--force && \
+git push --set-upstream origin master --force && \
 cd ${__CURRENT__}
